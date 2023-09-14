60e199fa5f80de02a5bbc12119c5f575e2cbbdfe
Natalie Amondi Akuro <102221931+NatalieAkuro@users.noreply.github.com>
2023-09-15 00:35:55 +0300

    schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+export const schemaTypes = [
+    user, pin, comment, postedBy, save
+]
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
